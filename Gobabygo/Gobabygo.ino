27d6aae6d5bd8ddfef0c534c7c1346c3439026c3
AaronMcGarry <aaronmcgarry42@gmail.com>
2020-04-30 20:20:26 -0500

    Code revisions after review

--- Gobabygo/Gobabygo.ino
+++ Gobabygo/Gobabygo.ino
@@ -1,7 +1,7 @@
 const int joyStickXPin (0);
-// const int joyStickYPin (1);
-const int ledPin (6);
-// const int leftMotor (4);
+const int joyStickYPin (1);
+const int rightMotorPin (6);
+const int leftMotorPin (4);
 const float deadzone = 0.1;
 
 unsigned long currentTime = 0;
@@ -9,51 +9,44 @@ unsigned long previousTime = 0;
 
 float joyXVal = 0;
 float scaledJoyXVal = 0.0;
-// int joyYVal = 0;
-// float scaledJoyYVal = 0.0;
+int joyYVal = 0;
+float scaledJoyYVal = 0.0;
 int ledVal = 0;
 float rightMotorVal = 0.0;
 float leftMotorVal = 0.0;
 
+void correctForDeadzone(float axis, float deadzone) {
+  if (abs(axis) <= deadzone){
+    axis = 0;
+  }
+  else {
+    if (axis > deadzone) {
+      axis = (axis - 1)/(1 - deadzone) + 1; //modified point-slope form of a line
+    }
+    else {
+      axis = (axis + 1)/(1 - deadzone) - 1;
+    }
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(rightMotorPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   joyXVal = analogRead(joyStickXPin);
   scaledJoyXVal = joyXVal/1023*2 - 1;
-  if (abs(scaledJoyXVal) <= deadzone){
-    scaledJoyXVal = 0;
-  }
-  else {
-    if (scaledJoyXVal > deadzone) {
-      scaledJoyXVal = (scaledJoyXVal - 1)/(1 - deadzone) + 1; //modified point-slope form of a line
-    }
-    else {
-      scaledJoyXVal = (scaledJoyXVal + 1)/(1 - deadzone) - 1;
-    }
-  }
+  correctForDeadzone(scaledJoyXVal, deadzone);
   
-  ledVal = map(joyXVal, 0, 1023, 0, 225);
-  analogWrite(ledPin, ledVal);
+  ledVal = map(joyXVal, 0, 1023, 0, 255);
+  analogWrite(rightMotorPin, ledVal);
   
-  // joyYVal = analogRead(joyStickYPin);
-  // scaledJoyYVal = 2/1023 * joyYval -1;
-  // if (abs(scaledJoyYVal) <= deadzone){
-  //   scaledJoyYVal = 0;
-  // }
-  // else {
-  //   if (scaledJoyYVal > deadzone) {
-  //     scaledJoyYVal = (scaledJoyYVal - 1)/(1 - deadzone) - 1;
-  //   }
-  //   else {
-  //     scaledJoyYVal = (scaledJoyYVal + 1)/(1 - deadzone) - 1;
-  //   }
-  // }
+  joyYVal = analogRead(joyStickYPin);
+  scaledJoyYVal = 2/1023 * joyYVal -1;
+  correctForDeadzone(scaledJoyYVal, deadzone);
 
   // "Loop" that happens twice every second
   currentTime = millis();
@@ -68,7 +61,5 @@ void loop() {
     // Serial.print("Y joystick value: ");
     // Serial.print(scaledJoyYVal);
     // Serial.println();
-
-    analogWrite(ledPin, ledVal);
   }
 }
